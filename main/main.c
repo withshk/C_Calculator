ce84396eed5bc63886ac4533f962b8614ea21798
withshk <withshk0527@gmail.com>
2026-06-25 08:33:15 +0900

    입력파트

---
+++ main/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main(void){
+    printf("=========계산기 프로그램=========\n\n");
+
+    double num1, num2;
+    char operator;
+
+    double result = 0;
+
+    int first = 1;
+
+    while(first){
+        printf("계산할 값을 입력하세요.\n");
+        if(scanf("%lf %c %lf", &num1, &operator, &num2) != 3){
+
+        }
+    }
+
+    return 0;
+}
